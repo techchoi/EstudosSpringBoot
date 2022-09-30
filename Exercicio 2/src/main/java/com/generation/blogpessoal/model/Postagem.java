0065f2b3c4c6e3b377deb0dfc8eac0ce4cdba429
techchoi <ninahjc.94@gmail.com>
2022-09-30 14:19:31 -0300

    atualizando model, controller

--- Exercicio 2/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ Exercicio 2/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -15,12 +15,12 @@ public class Postagem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank//(message = "O atributo título é Obrigatório!")
-    @Size(min = 5, max = 100) //message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+    @NotBlank(message = "O atributo título é Obrigatório!")
+    @Size(min = 5, max = 100,message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
     private String titulo;
 
-    @NotBlank//(message = "O atributo texto é Obrigatório!")
-    @Size(min =5, max = 100) //message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+    @NotBlank(message = "O atributo texto é Obrigatório!")
+    @Size(min =5, max = 100, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
     private String texto;
 
     @UpdateTimestamp
