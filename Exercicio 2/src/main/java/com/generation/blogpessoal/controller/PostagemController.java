57b965dd5e288522df4f942131f9cc076f9ee8ab
techchoi <ninahjc.94@gmail.com>
2022-10-03 10:06:52 -0300

    Criando getmapping id

--- Exercicio 2/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ Exercicio 2/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -3,17 +3,15 @@ package com.generation.blogpessoal.controller;
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/postagens")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*") //origem de acesso "*" = tudo, para não ficar limitado na porta 8080. Aqui é uma estrutura de framework
 public class PostagemController {
 
     @Autowired
@@ -21,5 +19,10 @@ public class PostagemController {
     @GetMapping
     public ResponseEntity<List<Postagem>> getAll(){
         return ResponseEntity.ok(postagemRepository.findAll());
+    }
+    @GetMapping("/{id}")//somente no id será necessário essa chaves por ser um identificador unico
+    public ResponseEntity<Postagem> getById(@PathVariable Long id){//-> simbulo de lambda
+        return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 }
--- Exercicio 2/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ Exercicio 2/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.generation.blogpessoal.model.Postagem;
 
-@Repository
+@Repository //essa interface vai ser utilizado como um repositorio
 public interface PostagemRepository extends JpaRepository<Postagem,Long> {
+
 }
